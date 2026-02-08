79d357bc28a0e96b744336e41105f652c2d1cdae
Rabbit <night31916@gmail.com>
2026-02-09 02:11:30 +0800

    adjust path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: import.meta.env.NODE_ENV === 'production' ? '/hex-react-task5/' : '/',
+    base: '/hex-react-task5/',
     plugins: [react()],
 });
